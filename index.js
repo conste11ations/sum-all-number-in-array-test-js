bdad35f6ac9395764e2bd07b96abf36191eda6f8
Rachel Kat <rachelkat@gmail.com>
2020-04-05 23:41:25 +0000

    completed index.js

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
-}
+const sumItems = function(array) {
+  
+  if (array.length === 0) {
+    return 0;
+  } else {
+    if (array.length === 1) {
+      if (Array.isArray(array[0])) {
+        return sumItems(array[0]) 
+      }
+      return array[0];
+    }
 
-module.exports = sumItems;
+    if (Array.isArray(array[0])) {
+      return sumItems(array[0]) + sumItems(array.slice(1)) ;
+    }
+  
+   return array[0] + sumItems(array.slice(1));
+  }
+}
